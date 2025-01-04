23d5352344bbdf3fcaa16cf8e23a68d7ea47b4bc
amanc1248 <amanc1248@gmail.com>
2025-01-04 16:05:57 +0545

    ASDF

--- constants/Query.js
+++ constants/Query.js
@@ -1,5 +1,6 @@
-const generateReportQuery = (startDate, endDate, interval, systemId, userId) => {
-    // Ensure dates are properly formatted for SQL
+const generateReportQuery = (startDate, endDate, interval, systemId, userId, subSystemIds) => {
+    const formattedSubSystemIds = subSystemIds.map(id => `'${id}'`).join(', ');
+
     return `
 DECLARE @startDate DATETIME = '${startDate}';
 DECLARE @endDate DATETIME = '${endDate}';
@@ -9,87 +10,50 @@ DECLARE @userId VARCHAR(50) = '${userId}';
 
 ;WITH IntervalData AS (
     SELECT 
-        DateAndTime,
+        DateTime,
         FT,
         TOTALIZER,
         PT,
+        LT,
         P_RunDay,
         P_RunHr,
         P_RunMn,
         userId,
         systemId,
-        DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateAndTime) / @interval) * @interval, @startDate) AS IntervalStart,
+        subSystemId,
+        DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateTime) / @interval) * @interval, @startDate) AS IntervalStart,
         ROW_NUMBER() OVER (
-            PARTITION BY DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateAndTime) / @interval) * @interval, @startDate)
-            ORDER BY DateAndTime
+            PARTITION BY 
+                DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateTime) / @interval) * @interval, @startDate),
+                subSystemId
+            ORDER BY DateTime
         ) AS RowNum
-    FROM TestFSN.dbo.maindata
-    WHERE DateAndTime >= @startDate AND DateAndTime <= @endDate
-    AND systemId = @systemId
-    AND userId = @userId
+    FROM DHADHIKOTEMAINDB.dbo.MainTable
+    WHERE DateTime >= @startDate 
+      AND DateTime < DATEADD(MINUTE, 1, @endDate) -- Adjusted to include the final minute
+      AND systemId = @systemId
+      AND userId = @userId
+      AND subSystemId IN (${formattedSubSystemIds})
 )
 SELECT
-    DateAndTime,
+    DateTime,
     FT,
     TOTALIZER,
     PT,
+    LT,
     P_RunDay,
     P_RunHr,
     P_RunMn,
     userId,
-    userId,
     systemId,
+    subSystemId,
     IntervalStart
-FROM IntervalData
-WHERE RowNum = 1
-ORDER BY DateAndTime;
+FROM IntervalData AS ID
+WHERE ID.RowNum = 1
+ORDER BY ID.DateTime;
    `;
 };
 
-
-
-
-
-
-// const generateReportQuery = (startDate, endDate, interval, systemId, userId) => {
-//     return `
-// DECLARE @startDate DATETIME = '${startDate}';
-// DECLARE @endDate DATETIME = '${endDate}';
-// DECLARE @interval INT = ${parseInt(interval, 10)};
-// DECLARE @systemId VARCHAR(50) = '${systemId}';
-// DECLARE @userId VARCHAR(50) = '${userId}';
-
-// ;WITH IntervalData AS (
-//     SELECT 
-//         DateAndTime,
-//         Flow,
-//         FT_Tot,
-//         Pressure,
-//         userId,
-//         systemId,
-//         DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateAndTime) / @interval) * @interval, @startDate) AS IntervalStart,
-//         ROW_NUMBER() OVER (
-//             PARTITION BY DATEADD(MINUTE, (DATEDIFF(MINUTE, @startDate, DateAndTime) / @interval) * @interval, @startDate)
-//             ORDER BY DateAndTime
-//         ) AS RowNum
-//     FROM TestFSN.dbo.maindata
-//     WHERE DateAndTime >= @startDate AND DateAndTime <= @endDate
-//     AND systemId = @systemId
-//     AND userId = @userId
-// )
-// SELECT
-//     DateAndTime,
-//     Flow,
-//     FT_Tot,
-//     Pressure,
-//     userId,
-//     systemId,
-//     IntervalStart
-// FROM IntervalData
-// WHERE RowNum = 1
-// ORDER BY DateAndTime;
-//    `;
-// };
 module.exports = {
     generateReportQuery,
 };
